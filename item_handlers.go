91b758375b163e2718f0d39208bad3ffde5fda2c
James Pants <james@rds.zone>
2018-06-16 18:45:09 -0400

    display

--- item_handlers.go
+++ item_handlers.go
@@ -1,31 +1,49 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+// Item is Individual Item, whether web or music project
 type Item struct {
 	Name        string `json:"item"`
 	Description string `json:"description"`
+	URL         string `json:"link"`
 }
 
+// PageData is each HTML template struct
+type PageData struct {
+	PageTitle string
+	Items     []*Item
+}
+
+// GetHandler displays all info
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 
+	// get all items in DB
 	items, err := store.GetItems()
 
-	fmt.Println(items)
-
-	itemsListBytes, err := json.Marshal(items)
+	// If we want to display as JSON
+	//itemsListBytes, err := json.Marshal(items)
 
 	if err != nil {
 		fmt.Println(fmt.Errorf("Error: %v", err))
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	w.Write(itemsListBytes)
 
-	//fmt.Fprintf(w, "Hello World!")
+	// Parse template
+	t := template.Must(template.ParseFiles("templates/layout.html"))
+
+	// create page data for template
+	data := PageData{
+		PageTitle: "James Pants",
+		Items:     items,
+	}
+
+	// run template
+	t.ExecuteTemplate(w, "layout", data)
 
 }
