f93c0c2a6de2089123b72de9385844338902aa5f
James Pants <james@rds.zone>
2018-06-18 22:42:48 -0400

    yo

--- item_handlers.go
+++ item_handlers.go
@@ -3,14 +3,21 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
+	"sort"
+
+	"github.com/BurntSushi/toml"
 )
 
 // Item is Individual Item, whether web or music project
 type Item struct {
-	Name        string `json:"item"`
-	Description string `json:"description"`
-	URL         string `json:"link"`
+	Description string
+	Title       string
+	URL         template.URL
+	Year        int
+	Type        string
 }
 
 // PageData is each HTML template struct
@@ -22,21 +29,53 @@ type PageData struct {
 // GetHandler displays all info
 func GetHandler(w http.ResponseWriter, r *http.Request) {
 
-	// get all items in DB
-	items, err := store.GetItems()
+	//var page PageData
+	var items []*Item
 
-	// If we want to display as JSON
-	//itemsListBytes, err := json.Marshal(items)
+	// Get all items in data foler
+	matches, err := filepath.Glob("./data/*.toml")
 
+	// Handle error
 	if err != nil {
 		fmt.Println(fmt.Errorf("Error: %v", err))
 		w.WriteHeader(http.StatusInternalServerError)
-		return
+	}
+
+	// For each toml file
+	for _, file := range matches {
+		// read the file as bytes
+		b, err := ioutil.ReadFile(file)
+
+		// Handle error
+		if err != nil {
+			fmt.Println(fmt.Errorf("Error: %v", err))
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		// Create an Item
+		var itemX Item
+
+		// Coerce to string
+		str := string(b)
+
+		// Decode TOML
+		if _, err := toml.Decode(str, &itemX); err != nil {
+			fmt.Println(fmt.Errorf("Error: %v", err))
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		// append item
+		items = append(items, &itemX)
+
 	}
 
 	// Parse template
 	t := template.Must(template.ParseFiles("templates/layout.html", "templates/style.html"))
 
+	// sort slice
+	sort.Slice(items, func(i, j int) bool {
+		return items[i].Year > items[j].Year
+	})
 	// create page data for template
 	data := PageData{
 		PageTitle: "James Pants",
--- main.go
+++ main.go
@@ -1,39 +1,21 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
-	_ "github.com/lib/pq"
 )
 
+// Main
 func main() {
 
 	fmt.Println("Starting server...")
 
-	// connect to db
-
-	connString := "dbname=jamespants sslmode=disable"
-	db, err := sql.Open("postgres", connString)
-
-	if err != nil {
-		panic(err)
-	}
-	err = db.Ping()
-
-	if err != nil {
-		panic(err)
-	}
-
-	InitStore(&dbStore{db: db})
-
-	fmt.Println("Connected to DB")
-
 	// declare new router
 	r := mux.NewRouter()
 
+	// handle home page
 	r.HandleFunc("/", GetHandler)
 
 	// handle static files
@@ -44,5 +26,7 @@ func main() {
 	r.PathPrefix("/static/").Handler(staticFileHandler).Methods("GET")
 
 	http.ListenAndServe(":8000", r)
+
+	fmt.Println("hi")
 
 }
